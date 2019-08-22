cc12793903849856b9096c0b0812590c40442216
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-08-22 10:39:40 +0900

    tutorial file add and commit

---
+++ application/controllers/Tutorial.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chris
+ * Date: 2019-08-22
+ * Time: 오전 10:38
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Tutorial extends CI_Controller{
+    public function index(){
+        echo "hello Index";
+    }
+    public function second(){
+        echo "hello Second";
+    }
+}
+
+?>
