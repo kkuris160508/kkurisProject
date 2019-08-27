f15ea4fca67b440cbc2708f97836473c23de2377
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-08-27 09:48:02 +0900

    _remap

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -8,11 +8,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * Time: 오전 9:24
  */
 class Product extends CI_Controller{
-
-    public function shoes($sandals, $id){
-        echo $sandals;
-        echo $id;
+//    public function _remap($method){
+//        if($method === 'some_method'){
+//            $this->$method();
+//        } else {
+//            $this->default_method();
+//        }
+//    }
+//
+    public function _remap($method, $param = array()){
+        $method = 'process_'.$method;
+        if(method_exists($this, $method)){
+            return call_user_func_array(array($this, $method), $param);
+        }
+        show_404();
     }
+
+//    public function shoes($sandals, $id){
+//        echo $sandals;
+//        echo $id;
+//    }
+//
+//
+
 }
 
 ?>
