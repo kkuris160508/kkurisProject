b7270e486ebe2a5574d3f9efdcff7902f8cf0a61
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-08-27 11:00:59 +0900

    모델 설정

--- application/controllers/Tutorial.php
+++ application/controllers/Tutorial.php
@@ -8,9 +8,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Tutorial extends CI_Controller{
     public function members(){
-        $this->load->model('Member_model');
-        $data['members'] = $this->Member_model->GetMembers();
-        $this->load->view('members', $data);
+        $this->load->model('Member_model'); //모델 로딩
+
+        $data['members'] = $this->Member_model->GetMembers(); //모델에 GetMembers 메서드를 사용
+
+        $this->load->view('Tutorial/members', $data); // 모델에서 가져온 값을 뷰에 전달달
     }
 }
 ?>
