e488493c647c13c9500708a45ab55bf2c3e14abd
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-08-29 13:35:16 +0900

    config setting

--- application/controllers/Constants.php
+++ application/controllers/Constants.php
@@ -12,8 +12,8 @@ class Constants extends CI_Controller{
         $dev_ip = array("211.52.72.51",
             "211.52.72.56",
             "211.52.72.59" );
-
-        return true;//in_array( $_SERVER["REMOTE_ADDR"], $dev_ip);
+        if($_SERVER['REMOTE_ADDR'])
+            return 1;//in_array( $_SERVER["REMOTE_ADDR"], $dev_ip);
     }
     function debug_var($var = '')
     {
