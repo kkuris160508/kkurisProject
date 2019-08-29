0b1899a85c306fac0ea4bf4ac313e0d986513b75
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-08-29 17:12:58 +0900

    autoload 에 debug_var 라이브러리 추가

--- application/views/Join/join_view_contents.php
+++ application/views/Join/join_view_contents.php
@@ -7,14 +7,20 @@
  */
 
 ?>
-    <p><?php echo $ID;?></p>
-    <input type="text" name="input_ID">
-    <p><?php echo $Password;?></p>
-    <input type="password" name="input_PW">
-    <p><?php echo $Email;?></p>
-    <input type="text" name="input_email">
+    <?php foreach($contents as $key => $value) :?>
+    <p><?php echo $value;?></p>
+    <input type="text" name="input_"+<?php echo $value?>>
+    <?php endforeach;?>
     <br>
 <button type="submit"><?php echo $btnName;?></button>
 
 <?php $result = $this->debug->debug_var($contents);?>
 <?php echo $result;?>
+
+
+<!--<input type="text" name="input_ID">-->
+<!--<p>--><?php //echo $Password;?><!--</p>-->
+<!--<input type="password" name="input_PW">-->
+<!--<p>--><?php //echo $Email;?><!--</p>-->
+<!--<input type="text" name="input_email">-->
+<!--<br>-->
