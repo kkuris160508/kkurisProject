9838fb8f0c96b71f08ac852f6be49108faa6c0c5
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-08-29 15:11:46 +0900

    config setting

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,4 +1,4 @@
-<?php
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
  * Created by PhpStorm.
  * User: chris
@@ -6,11 +6,12 @@
  * Time: 오후 2:21
  */
 
-class Test1 extends CI_Controller{
+class Test extends CI_Controller{
     public function index(){
         $this->load->library('Debug_var'); //데이터 검증 라이브러리 호출
 
         $this->Debug_var->test('chris');
     }
+
 }
 ?>
