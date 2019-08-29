a1344f80954624f2219f39616ef1839d70ba90e5
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-08-29 14:04:36 +0900

    config setting

--- application/controllers/Form.php
+++ application/controllers/Form.php
@@ -6,8 +6,9 @@
  * Time: 오전 11:28
  */
 class Form extends CI_Controller{
-
-    public function index(){
+    public $tmpVar = 1;
+    public function index($tmpVar1){
+        $tmpVar1 = 2;
         $this->load->helper(array('form', 'url'));
 
         $this->load->library(array('form_validation','DebugVar')); //데이터 검증 라이브러리 호출
@@ -17,6 +18,8 @@ class Form extends CI_Controller{
         } else {
             $this->load->view('formsuccess');
         }
+
+        $this->DebugVar->debug_var($tmpVar1);
 
     }
 }
