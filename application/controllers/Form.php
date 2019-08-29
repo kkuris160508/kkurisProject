9fc3211ed034c426dd6f5e915271b6b4ab56b79f
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-08-29 13:59:56 +0900

    config setting

--- application/controllers/Form.php
+++ application/controllers/Form.php
@@ -8,9 +8,9 @@
 class Form extends CI_Controller{
 
     public function index(){
-        $this->load->helper(array('form', 'url','MY_array'));
+        $this->load->helper(array('form', 'url'));
 
-        $this->load->library('form_validation'); //데이터 검증 라이브러리 호출
+        $this->load->library(array('form_validation','DebugVar')); //데이터 검증 라이브러리 호출
 
         if($this->form_validation->run() == FALSE){
             $this->load->view('myform');
