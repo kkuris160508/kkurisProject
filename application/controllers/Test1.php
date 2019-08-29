d3c65b358a8d7e91ec0e7bc87e6307ca02cb6980
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-08-29 14:23:54 +0900

    config setting

---
+++ application/controllers/Test1.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chris
+ * Date: 2019-08-29
+ * Time: 오후 2:21
+ */
+
+class Test1 extends CI_Controller{
+    public function testFn(){
+        $this->load->library('DebugVar'); //데이터 검증 라이브러리 호출
+
+        $this->DebugVar->test('chris');
+    }
+}
+?>
--- system/libraries/DebugVar.php
+++ system/libraries/DebugVar.php
@@ -6,7 +6,7 @@
  * Time: 오후 1:57
  */
 
-class CI_DebugVar{
+class DebugVar{
 //    function isDevDebug()
 //    {
 //
