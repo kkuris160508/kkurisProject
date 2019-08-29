f23e17c68e2618ec8fc1bb8c277ba497999e056b
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-08-29 15:37:45 +0900

    config setting

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -7,13 +7,16 @@
  */
 
 class Test extends CI_Controller{
+
     public function index(){
         $this->load->library('Debug'); //데이터 검증 라이브러리 호출
 
 //        $this->Debug_->test('chris');
-        $result = $this->debug->test();
+        $result = $this->debug->debug_var(); // 시발 debug 를 소문자로...ㅡㅡ
+
         echo $result;
     }
+
 
 }
 ?>
--- application/libraries/Debug.php
+++ application/libraries/Debug.php
@@ -11,4 +11,51 @@ class Debug{
     public function test($tmpVar = 'chris'){
         return "hi ".$tmpVar;
     }
+
+    function isDevDebug()
+    {
+
+        $dev_ip = array("211.52.72.51",
+            "211.52.72.56",
+            "211.52.72.59" );
+
+        return true;//in_array( $_SERVER["REMOTE_ADDR"], $dev_ip);
+    }
+    function debug_var($var = '')
+    {
+
+        if(isDevDebug())
+        {
+            echo _before();
+            if (is_array($var))
+            {
+                print_r($var);
+            }
+            elseif (is_object($var))
+            {
+                print_r($var);
+            }
+            else
+            {
+                echo $var;
+            }
+            echo _after();
+        }
+    }
+
+    function _before()
+    {
+        $before = '<div style="position:relative; z-index:999999; padding:10px 20px 10px 20px; background-color:#fbe6f2; border:1px solid #d893a1; color: #000; font-size: 12px;" class="Debug">'."\n";
+        $before .= '<h5 style="font-family:verdana,sans-serif; font-weight:bold; font-size:18px; margin:0px 0px 10px 0px;">Debug Helper Output</h5>'."\n";
+        $before .= '<xmp style="font-weight: bold;">'."\n";
+        return $before;
+    }
+
+    function _after()
+    {
+        $after = '</xmp>'."\n";
+        $after .= '<h5 style="font-family:verdana,sans-serif; font-weight:bold; font-size:18px; margin:0px 0px 10px 0px;">END</h5>'."\n";
+        $after .= '</div>'."\n";
+        return $after;
+    }
 }
