e68d4ce1df869f26de53d1f29f264a0326a235c3
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-08-30 14:18:05 +0900

    등록 버튼 클릭 시 작업 시작

--- application/controllers/Join.php
+++ application/controllers/Join.php
@@ -25,8 +25,12 @@ class Join extends CI_Controller{
     }
 
     public function joinOK($id, $email){
-        echo $id;
-        echo $email;
+//        echo $id;
+//        echo $email;
+
+        $query = $this->db->query("SELECT ID, PW, EMAIL FROM accountTB WHERE ID = '{$id}' AND EMAIL = '{$email}'");
+        echo $query;
+        //전달 받은 id, pw, email 을 DB 에 ID, pw, email 을 select 한 리턴 값과 비교 하여 맞으면 OK page, 아니면 오류? 다시? 페이지로
 
 //        $result = $this->debug->debug_var($data);
 //        echo $result;
--- application/views/Join/join_view_contents.php
+++ application/views/Join/join_view_contents.php
@@ -10,9 +10,9 @@
     <?php foreach($contents as $key => $value) :?>
     <p><?php echo $value;?></p>
         <?php if($key == '1'):?>
-            <input type="password" id = "<?php echo $value?>">
+            <input type="password" id = "<?php echo $value?>" placeholder="<?php echo $value?>">
         <?php else:?>
-            <input type="text" id = "<?php echo $value?>">
+            <input type="text" id = "<?php echo $value?>" placeholder="<?php echo $value?>">
         <?php endif;?>
         <input type="hidden" id = "hidden" value="<?php echo $value?>">
     <?php endforeach;?>
