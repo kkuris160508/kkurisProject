38db6fa46337aef304dd578e7aef9e1c2a859741
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-02 16:38:46 +0900

    write v 글작성 안되는 부분 수정

--- application/views/todo/write_contents_v.php
+++ application/views/todo/write_contents_v.php
@@ -5,19 +5,19 @@
                 <div class="control-group">
                     <label class="control-label" for="input01">내용</label>
                     <div class="controls">
-                        <input type="text" class="input-xlarge" id="input01" name="content">
+
                         <?php echo form_input('content')?>
                         <p class="help-block"></p>
                     </div>
                     <label class="control-label" for="input02">시작일</label>
                     <div class="controls">
-                        <input type="text" class="input-xlarge" id="input02" name="created_on">
+
                         <?php echo form_input('created_on')?>
                         <p class="help-block"></p>
                     </div>
                     <label class="control-label" for="input03">종료일</label>
                     <div class="controls">
-                        <input type="text" class="input-xlarge" id="input03" name="due_date">
+
                         <?php echo form_input('due_date')?>
                         <p class="help-block"></p>
                     </div>
