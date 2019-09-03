457cd8c540d28c67679ad44152ffabf60ff4e010
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-03 09:31:22 +0900

    페이지네이션

--- application/views/todo/list_contents_v.php
+++ application/views/todo/list_contents_v.php
@@ -1,38 +1,43 @@
-        <table cellspacing="0" cellpadding="0" class="table table-striped">
-            <thead>
-            <tr>
-                <th scope="col">번호</th>
-                <th scope="col">내용</th>
-                <th scope="col">시작일</th>
-                <th scope="col">종료일</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            foreach($list as $lt)
-            {
-                ?>
-                <tr>
-                    <th scope="row"><?php echo $lt -> board_id;?></th>
-                    <td><a rel="external" href="/bbs/<?php echo $this -> uri -> segment(1); ?>/view/<?php echo $this -> uri -> segment(3); ?>/<?php echo $lt -> board_id; ?>"> <?php echo $lt -> subject;?></a></td>
-                    <td><?php echo $lt -> user_name;?></td>
-                    <td><?php echo $lt -> hits;?></td>
-                    <td>
-                        <time datetime="<?php echo mdate("%Y-%M-%j", human_to_unix($lt -> reg_date)); ?>">
-                            <?php echo mdate("%Y-%M-%j", human_to_unix($lt -> reg_date));?>
-                        </time></td>
-                </tr>
-                <?php
-            }
-            ?>
-            </tbody>
-            <tfoot>
-            <tr>
-                <th colspan="4"><a href="/Main/write/" class="btn btn-success">
-                        쓰기
-                    </a></th>
-            </tr>
-            </tfoot>
-        </table>
-        <div><p></p></div>
-    </article>
+<table cellspacing="0" cellpadding="0" class="table table-striped">
+    <thead>
+    <tr>
+        <th scope="col">번호</th>
+        <th scope="col">내용</th>
+        <th scope="col">시작일</th>
+        <th scope="col">종료일</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach ($list as $lt) {
+        ?>
+        <tr>
+            <th scope="row">
+                <?php echo $lt -> id;?>
+            </th>
+            <td>
+                <a rel="external" href="/Main/view/<?php echo $lt -> id; ?>">
+                    <?php echo $lt -> content;?>
+                </a>
+            </td>
+            <td>
+                <time datetime="<?php echo mdate("%Y-%M-%j", human_to_unix($lt -> created_on)); ?>"><?php echo $lt -> created_on;?></time>
+            </td>
+            <td>
+                <time datetime="<?php echo mdate("%Y-%M-%j", human_to_unix($lt -> due_date)); ?>"><?php echo $lt -> due_date;?></time>
+            </td>
+        </tr>
+        <?php
+    }
+    ?>
+    </tbody>
+    <tfoot>
+    <tr>
+        <th colspan="4"><a href="/Main/write/" class="btn btn-success">
+                쓰기
+            </a></th>
+    </tr>
+    </tfoot>
+</table>
+<div><p></p></div>
+</article>
