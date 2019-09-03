0168c4c70f85e9b29e77049535fa897d72cb5960
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-03 13:31:28 +0900

    작성자 컬럼 추가

--- application/views/todo/list_contents_v.php
+++ application/views/todo/list_contents_v.php
@@ -52,14 +52,10 @@
                 <?php echo $lt -> account_id;?>
             </th>
             <td>
-                <a rel="external" href="/Main/view/<?php echo $lt -> id; ?>">
-                    <?php echo $lt -> subject;?>
-                </a>
+                <a rel="external" href="/Main/view/<?php echo $lt -> id; ?>"><?php echo $lt -> subject;?></a>
             </td>
             <td>
-                <a rel="external" href="/Main/view/<?php echo $lt -> id; ?>">
-                    <?php echo $lt -> content;?>
-                </a>
+                <a rel="external" href="/Main/view/<?php echo $lt -> id; ?>"><?php echo $lt -> content;?></a>
             </td>
             <td>
                 <time datetime="<?php echo mdate("%Y-%M-%j", human_to_unix($lt -> created_on)); ?>"><?php echo $lt -> created_on;?></time>
