1730b9873d70b4bc70fb9033528919ea84cffb5e
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-06 13:04:55 +0900

    write v 오류 수정

--- application/views/todo/write_contents_v.php
+++ application/views/todo/write_contents_v.php
@@ -6,8 +6,7 @@
                     <label class="control-label" for="input00">제목</label>
                     <div class="controls">
                         <!--                        <input type="text" class="input-xlarge" id="input01" name="content">-->
-                        <?php echo form_input('subject')?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
-                        <?php echo form_input('subject',set_value('subject'))?>
+                        <?php echo form_input('subject', set_value('subject'))?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
                         <p class="help-block">
                             <?php
                                 if(form_error('subject') == FALSE){
@@ -21,8 +20,7 @@
                     <label class="control-label" for="input01">내용</label>
                     <div class="controls">
 <!--                        <input type="text" class="input-xlarge" id="input01" name="content">-->
-                        <?php echo form_input('content')?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
-                        <?php echo form_input('content',set_value('content'))?>
+                        <?php echo form_input('content', set_value('content'))?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
                         <p class="help-block">
                             <?php
                             if(form_error('content') == FALSE){
@@ -36,8 +34,7 @@
                     <label class="control-label" for="input02">시작일</label>
                     <div class="controls">
 <!--                        <input type="text" class="input-xlarge" id="input02" name="created_on">-->
-                        <?php echo form_input('created_on', '',"placeholder='YYYY-MM-DD'")?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
-                        <?php echo form_input('created_on',set_value('created_on'))?>
+                        <?php echo form_input('created_on', set_value('created_on'),"placeholder='YYYY-MM-DD'")?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
                         <p class="help-block">
                             <?php
                             if(form_error('created_on') == FALSE){
@@ -51,8 +48,7 @@
                     <label class="control-label" for="input03">종료일</label>
                     <div class="controls">
 <!--                        <input type="text" class="input-xlarge" id="input03" name="due_date">-->
-                        <?php echo form_input('due_date','',"placeholder='YYYY-MM-DD'")?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
-                        <?php echo form_input('due_date',set_value('due_date'))?>
+                        <?php echo form_input('due_date',set_value('due_date'),"placeholder='YYYY-MM-DD'")?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
                         <p class="help-block">
                             <?php
                             if(form_error('due_date') == FALSE){
