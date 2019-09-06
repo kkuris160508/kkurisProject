4e194099a9340d70ec4c41a475fa2f2d06bb5ef3
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-06 17:29:28 +0900

    autoload.php session 사용하기 위해 설정 변경

--- application/views/todo/write_contents_v.php
+++ application/views/todo/write_contents_v.php
@@ -3,6 +3,12 @@
 <!--        <form class="form-horizontal" accept-charset="utf-8" method="post" action="" id="write_action">--> <!-- form open 사용으로 form 태그 삭제 -->
             <fieldset>
                 <div class="control-group">
+                    <?php
+                    $extraOpt = array(
+                        'placeholder'=>'YYYY-MM-DD',
+                        'onClick'=>'Chk_Text();'
+                    );
+                    ?>
                     <label class="control-label" for="input00">제목</label>
                     <div class="controls">
                         <!--                        <input type="text" class="input-xlarge" id="input01" name="content">-->
@@ -34,7 +40,8 @@
                     <label class="control-label" for="input02">시작일</label>
                     <div class="controls">
 <!--                        <input type="text" class="input-xlarge" id="input02" name="created_on">-->
-                        <?php echo form_input('created_on', set_value('created_on'),"placeholder='YYYY-MM-DD'")?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
+
+                        <?php echo form_input('created_on', set_value('created_on'),$extraOpt)?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
                         <p class="help-block">
                             <?php
                             if(form_error('created_on') == FALSE){
@@ -48,7 +55,7 @@
                     <label class="control-label" for="input03">종료일</label>
                     <div class="controls">
 <!--                        <input type="text" class="input-xlarge" id="input03" name="due_date">-->
-                        <?php echo form_input('due_date',set_value('due_date'),"placeholder='YYYY-MM-DD'")?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
+                        <?php echo form_input('due_date',set_value('due_date'),$extraOpt)?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
                         <p class="help-block">
                             <?php
                             if(form_error('due_date') == FALSE){
@@ -68,3 +75,19 @@
 <!--        </form>-->
         <?php echo form_close();?>
     </article>
+
+        <SCRIPT LANGUAGE="JavaScript">
+            <!--
+            function Chk_Text(){
+
+                var ChkText=/^([a-zA-Z0-9]{1,20})$/
+
+                if(ChkText.test(ChkID)==false){
+                    alert("'"+ChkID + "' 는 사용이 불가능 합니다. \n 영문문자나 숫자 아이디만 사용이 가능합니다.")
+                    return;
+                }
+            }
+
+            //-->
+        </SCRIPT>
+
