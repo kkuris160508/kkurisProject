1d80bd88baaa637b57e55d69167bda5f732d406c
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-06 11:22:41 +0900

    lang 변경

--- system/language/english/form_validation_lang.php
+++ system/language/english/form_validation_lang.php
@@ -40,7 +40,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$lang['form_validation_required']		= 'The {field} field is required.';
 $lang['form_validation_required']		= '{field} 항목이 입력되어야 합니다.';
 $lang['form_validation_isset']			= 'The {field} field must have a value.';
-$lang['form_validation_valid_email']		= 'The {field} field must contain a valid email address.';
+$lang['form_validation_valid_email']		= '{field} 형식을 확인해주세요. <br> ex) xxxx@xxxx.com, co.kr ..';
 $lang['form_validation_valid_emails']		= 'The {field} field must contain all valid email addresses.';
 $lang['form_validation_valid_url']		= 'The {field} field must contain a valid URL.';
 $lang['form_validation_valid_ip']		= 'The {field} field must contain a valid IP.';
@@ -56,7 +56,7 @@ $lang['form_validation_is_numeric']		= 'The {field} field must contain only nume
 $lang['form_validation_integer']		= 'The {field} field must contain an integer.';
 $lang['form_validation_regex_match']		= 'The {field} field is not in the correct format.';
 //$lang['form_validation_matches']		= 'The {field} field does not match the {param} field.';
-$lang['form_validation_matches']		= '{param} 과 동일한 비밀번호를 입력하십시오.';
+$lang['form_validation_matches']		= '{filed} 가 {param} 와(과) 다릅니다. 동일한 {param} 를 입력하십시오.';
 $lang['form_validation_differs']		= 'The {field} field must differ from the {param} field.';
 $lang['form_validation_is_unique'] 		= 'The {field} field must contain a unique value.';
 $lang['form_validation_is_natural']		= 'The {field} field must only contain digits.';
