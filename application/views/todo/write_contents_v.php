d12208b8b7c0893db4bcb462bdc6799224d6d291
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-09 17:00:34 +0900

    로그인 뷰 수정

--- application/views/todo/write_contents_v.php
+++ application/views/todo/write_contents_v.php
@@ -42,7 +42,7 @@
                             </p>
                         </div>
                     </div>
-                    <div = 'endDateDiv'>
+                    <div id = 'endDateDiv'>
                         <label class="control-label" for="input03">종료일</label>
                         <div class="controls">
                             <!--                        <input type="text" class="input-xlarge" id="input03" name="due_date">-->
