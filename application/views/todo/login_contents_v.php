1481fcebe57a4152a3a3376e90d4006e4ddec6a8
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-09 15:03:18 +0900

    회원가입시 한글 불가

--- application/views/todo/login_contents_v.php
+++ application/views/todo/login_contents_v.php
@@ -6,20 +6,44 @@
         <label class="control-label" for="input00">아이디</label>
         <div class="controls">
             <!--                        <input type="text" class="input-xlarge" id="input01" name="content">-->
-            <?php echo form_input('account_id')?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
-            <p class="help-block"></p>
+            <?php echo form_input('account_id', set_value('account_id'))?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
+            <p class="help-block">
+                <?php
+                if(form_error('account_id') == FALSE){
+                    echo "";
+                } else {
+                    echo form_error('account_id');
+                }
+                ?>
+            </p>
         </div>
         <label class="control-label" for="input01">패스워드</label>
         <div class="controls">
             <!--                        <input type="text" class="input-xlarge" id="input01" name="content">-->
             <?php echo form_password('Password')?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
-            <p class="help-block"></p>
+            <p class="help-block">
+                <?php
+                if(form_error('Password') == FALSE){
+                    echo "";
+                } else {
+                    echo form_error('Password');
+                }
+                ?>
+            </p>
         </div>
         <label class="control-label" for="input02">이메일</label>
         <div class="controls">
             <!--                        <input type="text" class="input-xlarge" id="input02" name="created_on">-->
-            <?php echo form_input('EMAIL', '',"placeholder='xxx@xxx.xxx'")?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
-            <p class="help-block"></p>
+            <?php echo form_input('EMAIL', set_value('EMAIL'),"placeholder='xxx@xxx.xxx'")?> <!-- form open 사용해야함 csrf protection TRUE 일 때 POST 방식으로 DB 접근권한을 한번 막기때문에 form open 사용-->
+            <p class="help-block">
+                <?php
+                if(form_error('EMAIL') == FALSE){
+                    echo "";
+                } else {
+                    echo form_error('EMAIL');
+                }
+                ?>
+            </p>
         </div>
 
         <div class="form-actions">
