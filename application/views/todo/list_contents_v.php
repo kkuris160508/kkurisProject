3c26ddc6ef9d2957d59cdaee34f40e121329531d
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-11 14:59:40 +0900

    글 검색 기능 추가 시작

--- application/views/todo/list_contents_v.php
+++ application/views/todo/list_contents_v.php
@@ -6,7 +6,7 @@
 ?>
 <?php echo form_open('/Main/searchText')?>
 </header>
-<table class="table table-striped">
+<table class="table table-striped" style="align-content: center">
     <thead class="thead-dark">
     <tr>
         <th scope="col">번호</th>
