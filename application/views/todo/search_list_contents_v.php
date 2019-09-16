396bd8bcfa5bc72b6e9d4dbe7424102e02b3a26c
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-16 17:15:02 +0900

    글 검색 에서 페이징네이션 시작

--- application/views/todo/search_list_contents_v.php
+++ application/views/todo/search_list_contents_v.php
@@ -74,7 +74,7 @@ $opt = array(
             <option value = 'account_id' <?php echo set_select('selectCategory','account_id'); ?>>작성자</option>
         </select>
     </td>
-    <td colspan = '6' style="width:800px"><?php echo form_input('searchTxt','',"placeholder='검색어를 입력하세요'; style = 'width:850px;'")?></td>
+    <td colspan = '6' style="width:800px"><?php echo form_input('searchTxt',set_value('searchTxt'),"placeholder='검색어를 입력하세요'; style = 'width:850px;'")?></td>
     <td> <div class="form-actions"><input type ='submit' class="btn btn-primary" id = 'searchLists' value="검색"></div></td>
     <!-- input 영역 추가 -->
     </tr>
