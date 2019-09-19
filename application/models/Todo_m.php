1b6e0061bc61610252df9be9c9f0ddfd499ad3bf
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-19 10:09:21 +0900

    글 작성 시 상태 값 추가 시작

--- application/models/Todo_m.php
+++ application/models/Todo_m.php
@@ -109,7 +109,8 @@ class Todo_m extends CI_Model{
     function insert_todo($subject, $content, $created_on, $due_date, $writer, $status){
         $sql = "INSERT INTO items (subject, content, created_on, due_date, writer, writetime, status) 
                 VALUES ('" .$subject. "','" .$content. "','".$created_on."','".$due_date. "' , '".$writer. "',now()), '$status'";
-        $query = $this->db->query($sql); // return 없고 insert 이후 완료. 결과는 컨트롤러에서 받음.
+        echo $sql;
+//        $query = $this->db->query($sql); // return 없고 insert 이후 완료. 결과는 컨트롤러에서 받음.
     }
 
     function delete_todo($id, $no){
