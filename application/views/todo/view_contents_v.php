494acce77078a3f699a3d3cd9da7ea62032076ee
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-19 16:59:20 +0900

    글 작성 시 상태 값 추가 시작

--- application/views/todo/view_contents_v.php
+++ application/views/todo/view_contents_v.php
@@ -61,9 +61,7 @@
                     <?php
                         if($accountInfo == $transAccountID){?>
                             <a href="http://34.80.199.17/Main/edit/<?php echo $this -> uri -> segment(3);?>>" class="btn btn-success">수정</a>
-                        <?} else {?>
-                            <a class="btn btn-primary" onclick="alert('수정불가');">수정</a>
-                        <?}?>
+
                 </th>
             </tr>
             </tfoot>
