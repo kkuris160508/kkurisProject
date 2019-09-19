787f10d4f1fa714cd27d29b990f8829c15f1ae5f
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-19 11:31:40 +0900

    글 작성 시 상태 값 추가 시작

--- application/views/todo/edit_contents_v.php
+++ application/views/todo/edit_contents_v.php
@@ -1,8 +1,5 @@
 <?php echo form_open('/Main/edit')?>
-<?
-$result2 = $this->debug->debug_var($views);
-echo $result2;
-?>
+
 <table class="table table-striped">
     <thead class="thead-dark">
     <tr>
