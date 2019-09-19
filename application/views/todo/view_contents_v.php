bffe4e2177a30228941a5148d04d2ff58b3c2533
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-19 16:56:32 +0900

    글 작성 시 상태 값 추가 시작

--- application/views/todo/view_contents_v.php
+++ application/views/todo/view_contents_v.php
@@ -59,7 +59,7 @@
                     <a href="http://34.80.199.17/Main/delete/<?php echo $this -> uri -> segment(3); ?>/<?php echo $transID; ?>/<?php echo $transAccountID; ?>" class="btn btn-danger">삭제</a>
                     <a href="http://34.80.199.17/Main/write/" class="btn btn-success">쓰기</a>
                     <?php
-                        if($accountInfo == $transAccountID)?><a href="http://34.80.199.17/Main/edit/>" class="btn btn-success">수정</a>
+                        if($accountInfo == $transAccountID)?><a href="http://34.80.199.17/Main/edit/<?php echo $this -> uri -> segment(3);?>>" class="btn btn-success">수정</a>
 <!--                    --><?php
 //                        else?><!--<a class="btn btn-primary" onclick="alert('수정불가');">-->
                 </th>
