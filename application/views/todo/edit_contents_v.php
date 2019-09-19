ffa31c9d2f924afe4a1b4d19100b68a0c479a7d5
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-19 17:53:13 +0900

    글 작성 시 상태 값 추가 시작

--- application/views/todo/edit_contents_v.php
+++ application/views/todo/edit_contents_v.php
@@ -51,7 +51,7 @@
         <th rowspan="2">내용</th>
         <th colspan="6" name = 'contents'>
             <?php $tmpContent = $views -> content;?>
-            <?php echo form_input('content',set_value('contents'),"placeholder='{$tmpContent}'")?>
+            <?php echo form_input('content',set_value($tmpContent),"placeholder='{$tmpContent}'")?>
         </th>
     </tr>
 
