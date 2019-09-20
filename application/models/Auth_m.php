02442ca22de35d3ca1f472382e3fbb44f8229e30
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-20 09:49:47 +0900

    글 작성 시 상태 값 추가 시작

--- application/models/Auth_m.php
+++ application/models/Auth_m.php
@@ -12,7 +12,9 @@ class Auth_m extends CI_Model{
     }
 
     public function login($auth){
-        $sql = "SELECT * FROM accountTB where account_id = '" . $auth['account_id']. "' AND PW = '" . $auth['PW']. "' ";
+//        $sql = "SELECT * FROM accountTB where account_id = '" . $auth['account_id']. "' AND PW = '" . $auth['PW']. "' ";
+        $sql = "SELECT * FROM accountTB WHERE account_id = {$auth['account_id']} AND PW = password({$auth['PW']})";
+
 
         $query = $this->db->query($sql);
 
