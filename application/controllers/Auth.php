45a2a60924e12daa3ddc3ec8715bb4bf485ed97d
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-20 10:52:04 +0900

    hash_pw

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -39,15 +39,15 @@ class Auth extends CI_Controller{
 
             $tmpRes = $this->Auth_m->getPW($auth_data['account_id']);
 
-            $result2 = $this->debug->debug_var($tmpRes); // debug 를 소문자로...ㅡㅡ
-            echo $result2;
+//            $result2 = $this->debug->debug_var($tmpRes); // debug 를 소문자로...ㅡㅡ
+//            echo $result2;
 
             //진행되기전 DB에서 PW 가지고 오는 모델 로드
-//            if(password_verify($auth_data['PW'], $tmpHash_1)){
-//                echo 'password is vaild';
-//            } else {
-//                echo 'password is wrong';
-//            }
+            if(password_verify($auth_data['PW'], $tmpRes)){
+                echo 'password is vaild';
+            } else {
+                echo 'password is wrong';
+            }
 
 //            $auth_data['PW'] = password_hash($auth_data['PW'],1);
 //            echo $auth_data['PW'];
