56bc1e91bfc3793c3ded044f277e41daf13e3c48
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-09-27 17:24:18 +0900

    파일 업로드

--- application/views/view_file_v.php
+++ application/views/view_file_v.php
@@ -1,9 +1,9 @@
 <html>
 <body>
-<?php var_dump($list);?>
-<?php foreach($list as $item => $fileName):?>
+<?php //var_dump($list);?>
+<?php foreach($list as $item):?>
 <p>
-    <img src="/34.80.199.17/uploads/<?php echo $fileName?>" width="245">
+    <img src="/34.80.199.17/uploads/<?php echo $item -> file_name?>" width="245">
 </p>
 <?php endforeach;?>
 </body>
