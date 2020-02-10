9da47d6a3cfbe12dc959ef0f476f4c677b67369e
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2020-02-10 14:33:32 +0900

    lotto view 생성

--- application/views/lotto/lotto_contents_v.php
+++ application/views/lotto/lotto_contents_v.php
@@ -1,8 +1,11 @@
 
+<?php foreach ($views as $num) : ?>
 
-<?php echo $views -> num_1;?> 1
-<?php //echo $views -> num_2;?><!-- 2-->
-<?php //echo $views -> num_3;?><!-- 3-->
-<?php //echo $views -> num_4;?><!-- 4-->
-<?php //echo $views -> num_5;?><!-- 5-->
-<?php //echo $views -> num_6;?><!-- 6-->
+    <?php echo $num -> num_1;?> 1
+    <?php //echo $views -> num_2;?><!-- 2-->
+    <?php //echo $views -> num_3;?><!-- 3-->
+    <?php //echo $views -> num_4;?><!-- 4-->
+    <?php //echo $views -> num_5;?><!-- 5-->
+    <?php //echo $views -> num_6;?><!-- 6-->
+
+<?php endforeach;?>
