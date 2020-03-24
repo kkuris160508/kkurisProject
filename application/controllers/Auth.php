c416b8bdd0121cd58b8de92e723d350782aa69e4
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2020-03-24 17:52:13 +0900

    이전 CI 페이지 원복

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -59,6 +59,7 @@ class Auth extends CI_Controller{
 
             } else {
                 $this->debug->debug_var($tmpRes); // debug 를 소문자로...ㅡㅡ
+                $this->debug->debug_var($auth_data); // debug 를 소문자로...ㅡㅡ
 
 //                alert('아이디나 비밀번호를 확인해 주세요.', '/Main/lists');
 //                exit;
---
+++ application/helpers/debug_helper.php
@@ -0,0 +1,53 @@
+<?php
+function callStack() {
+    ob_start();
+    $stacktrace = debug_backtrace();
+    print str_repeat("=", 50) ."<br>\n";
+    $i = 0;
+    foreach($stacktrace as $node) {
+        if($i==0)
+        {
+            $i++;
+            continue;
+        }
+        print "$i. ".basename($node['file']) .":" .$node['function'] ."(" .$node['line'].")<br>\n";
+        $i++;
+    }
+    $output = ob_get_contents();
+    ob_end_clean();
+
+    return $output;
+}
+function isDevDebug()
+{
+    $dev_ip = array("211.52.72.51",
+        "211.52.72.36",
+        "211.52.72.59",             //류정현
+        "211.52.72.56"
+    );
+
+    return in_array( $_SERVER["REMOTE_ADDR"], $dev_ip);
+}
+function debug()
+{
+    if (isDevDebug())
+    {
+        $sapi_name = php_sapi_name();
+        $args = func_get_args();
+        $style2 = "word-break:break-all;word-wrap:break-word;";
+        $style = "margin:5px 0px 5px 0px;color:white;background-color:black;line-height:1.5em;font-size:11px;font-family:Verdana;padding:5px;text-align:left;float:none;clear:both;display:block;position: static;";
+        if (preg_match('/msie ([\d\.]+)/i', $_SERVER['HTTP_USER_AGENT'])) $style2 = "word-break:break-all;word-wrap:break-word;";
+        $_DEBUG_ID = 'nKDebug';
+        /*스타일 적용*/
+        if (is_string($args[0])) {
+            if (preg_match('/^(style=)(.+)/i', $args[0])) {
+                $style .= str_replace(array('\'', "\"", "style="), '', array_shift($args));
+            } else if (preg_match('/^(id=)(.+)/i', $args[0])) {
+                $_DEBUG_ID = str_replace(array('\'', "\"", "style="), '', array_shift($args));
+            }
+        }
+        if ($sapi_name != 'cli') echo "\n<!--{{" . $_DEBUG_ID . "}}-->\n<div style=\"$style\">\n<xmp style='$style2;white-space:pre-wrap;'>";
+        print_r(count($args) > 1 ? $args : $args[0]);
+        if ($sapi_name != 'cli') echo "</xmp></div>\n";
+    }
+}
